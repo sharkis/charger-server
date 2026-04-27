6f4ed62445ab024bef8d35b65341f6f837e651bd
Albert Sharkis <sharkis@sharkis.org>
2026-04-26 21:20:36 -0600

    read file from fs

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+const {open} = require('node:fs/promises');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
+let imagedata;
+	// load image from fs
+(async ()=>{
+	let ih  = await open('./images/image.raw');
+	imagedata = await ih.readFile();
+	console.log("image loaded");
+})();
 app.get('/', (req,res)=>{
-	res.send("Hello world!");
+	console.log("sending data");
+	// send raw to client
+	res.send(imagedata);
 });
 app.listen(port,'0.0.0.0',()=>{
 	console.log(`example app listening on port ${port}`);
